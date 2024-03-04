6fa6ab3605ccaee4415d6a67087fae403e26afea
Mink7891 <60268613+Mink7891@users.noreply.github.com>
2024-03-04 16:47:18 +0300

    Lesson1

---
+++ Lesson1/settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lesson1"
+include(":app")
+include(":layouttype")
+include(":buttonclicker")
+include(":controllesson1")
+include(":controllesson12")
+include(":controlleson1")
