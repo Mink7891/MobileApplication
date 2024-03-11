a573b57580b0edbc92357fbc0d0e3a3fbe281f1a
Mink7891 <60268613+Mink7891@users.noreply.github.com>
2024-03-11 09:18:42 +0300

    lab3

---
+++ Lesson3/intentapp/src/main/java/ru/mirea/logunovao/intentapp/SecondActivity.java
@@ -0,0 +1,32 @@
+package ru.mirea.logunovao.intentapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import ru.mirea.logunovao.intentapp.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        String text = (String) getIntent().getSerializableExtra("key");
+        TextView tv = findViewById(R.id.textView);
+
+        tv.setText(text);
+    }
+}
